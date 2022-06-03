98b58ea7a9ee82e512cbb39bb4dcb6c31896fb85
seongmin221 <seongmin221@naver.com>
2022-06-03 12:27:10 +0900

    initial commit

---
+++ liveOn/liveOn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  liveOn
+//
+//  Created by 이성민 on 2022/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ liveOn/liveOn/liveOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  liveOnApp.swift
+//  liveOn
+//
+//  Created by 이성민 on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct liveOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
