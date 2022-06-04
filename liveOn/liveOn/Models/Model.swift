f6b9073647dc7a40f9d9a71049b63bb8443823f5
seongmin221 <seongmin221@naver.com>
2022-06-04 09:46:12 +0900

    build MVVM model

---
+++ liveOn/liveOn/Models/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  liveOn
+//
+//  Created by 이성민 on 2022/06/04.
+//
+
+import Foundation
---
+++ liveOn/liveOn/ViewModels/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  liveOn
+//
+//  Created by 이성민 on 2022/06/04.
+//
+
+import Foundation
