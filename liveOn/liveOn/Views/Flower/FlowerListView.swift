19130a36dd6d4a0f3d02b4d784bf0de74a96ce2f
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-06-12 00:36:21 +0900

    [Feat] Create FlowerListView
    임시로 꽃병과 그 안에 들어가는 꽃들을 만들어 두었습니다.

--- liveOn/liveOn/Views/Flower/FlowerListView.swift
+++ liveOn/liveOn/Views/Flower/FlowerListView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct FlowerListView: View {
+    
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        
+        ZStack {
+            
+            // 꽃병
+            Circle()
+                .foregroundColor(.gray)
+                .opacity(0.36)
+                .padding()
+            
+            LazyVGrid(columns: [GridItem(), GridItem(), GridItem()], spacing: 42) {
+                ForEach(0..<9, id: \.self) { value in
+                    ZStack {
+                        Image("flower")
+                            .resizable()
+                            
+                    }
+                    .frame(width: UIScreen.main.bounds.width * 0.3, height: UIScreen.main.bounds.width * 0.18)
+                }
+            }
+            .padding()
+            
+        } // ZStack
+        .navigationTitle("꽃")
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.background)
+        
+    } // body
 }
 
 struct FlowerListView_Previews: PreviewProvider {
