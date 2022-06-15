fa395029e09b948719f402a57f68ed633cfd56ec
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-06-15 16:46:05 +0900

    Feat: PillView Making

--- liveOn/liveOn/Views/Pill/Pill.swift
+++ liveOn/liveOn/Views/Pill/Pill.swift
@@ -15,14 +15,17 @@ class Pill: Identifiable, ObservableObject {
     let color: String
     let effect: String
     let prescribedDate: String
+    let sender: String
     
-    init(name: String, color: String, effect: String, prescribedDate: String) {
+    init(name: String, color: String, effect: String, prescribedDate: String, sender: String) {
         
         id = UUID()
         self.name = name
         self.color = color
         self.effect = effect
         self.prescribedDate = prescribedDate
+        self.sender = sender
+        
     }
 }
 
@@ -32,87 +35,103 @@ let pillList: [Pill] = [
     Pill(
         name: "박카스",
         color: "갈색",
-        effect: "호랑이 기운이 솟아나진 않지만 조금 나아집니다.",
-        prescribedDate: "220614"),
+        effect: "박카스 한 병과 함께 솟아나는 호랑이 기운!",
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "쏠라C",
         color: "노란색",
-        effect: "시큼합니다.",
-        prescribedDate: "220614"),
+        effect: "시큼하고 달달한 맛에 기운이 솟아납니다.",
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "텐텐",
         color: "붉은색",
         effect: "추억이 되살아납니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "마그네슘",
         color: "흰색",
         effect: "눈 밑 떨림을 해소합니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "박카스",
         color: "갈색",
-        effect: "호랑이 기운이 솟아나진 않지만 조금 나아집니다.",
-        prescribedDate: "220614"),
+        effect: "박카스 한 병과 함께 솟아나는 호랑이 기운!",
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "쏠라C",
         color: "노란색",
-        effect: "시큼합니다.",
-        prescribedDate: "220614"),
+        effect: "시큼하고 달달한 맛에 기운이 솟아납니다.",
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "텐텐",
         color: "붉은색",
         effect: "추억이 되살아납니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "마그네슘",
         color: "흰색",
         effect: "눈 밑 떨림을 해소합니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "박카스",
         color: "갈색",
-        effect: "호랑이 기운이 솟아나진 않지만 조금 나아집니다.",
-        prescribedDate: "220614"),
+        effect: "박카스 한 병과 함께 솟아나는 호랑이 기운!",
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "쏠라C",
         color: "노란색",
-        effect: "시큼합니다.",
-        prescribedDate: "220614"),
+        effect: "시큼하고 달달한 맛에 기운이 솟아납니다.",
+        prescribedDate: "220614",
+        sender: "재헌"),
     Pill(
         name: "텐텐",
         color: "붉은색",
         effect: "추억이 되살아납니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "마그네슘",
         color: "흰색",
         effect: "눈 밑 떨림을 해소합니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "박카스",
         color: "갈색",
-        effect: "호랑이 기운이 솟아나진 않지만 조금 나아집니다.",
-        prescribedDate: "220614"),
+        effect: "박카스 한 병과 함께 솟아나는 호랑이 기운!",
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "쏠라C",
         color: "노란색",
-        effect: "시큼합니다.",
-        prescribedDate: "220614"),
+        effect: "시큼하고 달달한 맛에 기운이 솟아납니다.",
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "텐텐",
         color: "붉은색",
         effect: "추억이 되살아납니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "마그네슘",
         color: "흰색",
         effect: "눈 밑 떨림을 해소합니다.",
-        prescribedDate: "220614"),
+        prescribedDate: "220614",
+        sender: "유진"),
     Pill(
         name: "진통제",
         color: "빨간색",
         effect: "통증을 가라앉힙니다.",
-        prescribedDate: "220614")
-    
+        prescribedDate: "220614",
+        sender: "재헌")
 ]
--- liveOn/liveOn/Views/Pill/PillListView.swift
+++ liveOn/liveOn/Views/Pill/PillListView.swift
@@ -1,34 +1,59 @@
+//
+//  PillListView.swift
+//  liveOn
+//
+//  Created by 김보영 on 2022/06/13.
+//
+
 import SwiftUI
 
 struct PillListView: View {
-    @Environment(\.dismiss) private var dismiss
+    
+    @State var showPillPopUp = false
+    @State var clickedPillIndex = 0
+    
     var body: some View {
-        
-        ScrollView {
-            
-            LazyVGrid(columns: [GridItem(), GridItem()], spacing: 10) {
-                
-                //  n
-                ForEach(0..<pillList.count, id: \.self) { index in
-                    
-                    PillCardView(content: pillList[index])
-                    
-                }
-            }
-        }
-        .navigationToBack(dismiss)
-        .navigationTitle("약")
-        .navigationBarTitleDisplayMode(.inline)
-        .padding(.horizontal)
-    } // body
-}
 
-struct PillListView_Previews: PreviewProvider {
-    static var previews: some View {
+            ScrollView {
+                ZStack {
+                    VStack(alignment: .center) {
+                        LazyVGrid(columns: [GridItem(), GridItem()], spacing: 12) {
+                            
+                            ForEach(0..<pillList.count, id: \.self) { index in
+                                
+                                PillCardView(content: pillList[index])
+                                    .onTapGesture {withAnimation(.linear(duration: 0.4)) {
+                                        showPillPopUp = true
+                                        clickedPillIndex = index
+                                        
+                                    }
+                                    }
+                                /*
+                                 클릭이 가능한 것들은 스크롤뷰 안에 들어갈 수 없습니까???
+                                 버튼으로도 시도해보고 온탭제스쳐로도 해봤는데 둘 다 스크롤 뷰를 똥으로 만들어버림
+                                 */
+                                
+                            } // ForEach
+                        } // LazyVGrid
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    }
+                    
+                    Color.white.opacity(showPillPopUp ? 0.6 : 0).edgesIgnoringSafeArea(.all)
+                    
+                    if showPillPopUp {
+                        // show Pill effect PopUp
+                        
+                        PillPopUpView(showPillPopUp: $showPillPopUp, indexOfCard: $clickedPillIndex)
+                        
+                    }
+                } // ZStack
+                
+            }// ScrollView
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .navigationTitle("약")
+            .navigationBarTitleDisplayMode(.inline)
         
-        PillListView()
-        
-    }
+    }// body
 }
 
 struct PillCardView: View {
@@ -39,39 +64,75 @@ struct PillCardView: View {
         
         ZStack {
             
-            RoundedRectangle(cornerRadius: 16)
+            RoundedRectangle(cornerRadius: 4)
                 .foregroundColor(.white)
+                .frame(width: 168)
                 .shadow(color: .gray, radius: 12, x: 1, y: 1)
                 .opacity(0.14)
             
             VStack(alignment: .center) {
                 
-                Image("medicine")
-                
-                VStack(alignment: .center) {
+                ZStack {
                     
-                    // MARK: 약 이름
                     ZStack {
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke()
-                            .frame(width: CGFloat(content.name.count) * 16 + 8, height: 24, alignment: .center)
-                            .foregroundColor(.green)
+                        Image("medicine")
+                            .scaledToFit()
                         
-                        Text("\(content.name)")
-                            .foregroundColor(.green)
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .opacity(0.6)
                         
                     } // ZStack
                     
-                    Text("\(content.prescribedDate)")
-                        .foregroundColor(.green)
-                        .font(.system(size: 12))
-                    
-                } // VStack
+                    VStack(alignment: .center) {
+                        
+                        ZStack(alignment: .center) {
+                            
+                            RoundedRectangle(cornerRadius: 12)
+                                .frame(width: CGFloat(content.name.count) * 16 + 8, height: 24, alignment: .center)
+                                .foregroundColor(.green)
+                            
+                            Text("\(content.name)")
+                                .foregroundColor(.white)
+                            
+                        } // ZStack
+                        
+                        ZStack {
+                            
+                            RoundedRectangle(cornerRadius: 12)
+                                .frame(width: 72, height: 24, alignment: .center)
+                                .foregroundColor(.white)
+                                .opacity(0.7)
+                            
+                            Text("\(content.prescribedDate)")
+                                .foregroundColor(.green)
+                                .font(.system(size: 14))
+                            
+                        } // ZStack
+                    } // VStack
+                } // ZStack
+                
+                Divider()
+                
+                Text("\(content.sender)약국")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.green)
+                    .font(.system(size: 16))
+                
             } // VStack
             .padding(.bottom, 12)
             
         } // ZStack
-        .padding(.horizontal, 2)
+        .padding(.horizontal)
+        
+    } // body
+}
+
+// MARK: Preview
+struct PillListView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        PillListView()
         
     }
 }
