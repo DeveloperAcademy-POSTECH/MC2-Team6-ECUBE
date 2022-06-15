2016b4f98f8ec5b065b91213f1d4f7db8ee89322
italsehf <alsehfdl0316@naver.com>
2022-06-15 14:27:19 +0900

    [Feat] Add DateExtention

--- liveOn/liveOn/Extensions/DateExtension.swift
+++ liveOn/liveOn/Extensions/DateExtension.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 // Extending Date to get Current Month Dates
-// extension Date {
-//    
-//    func getAllDates() -> [Date] {
-//        
-//        let calendar = Calendar.current
-//        
-//        // getting start Date
-//        let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
-//        
-//        let range = calendar.range(of: .day, in: .month, for: startDate)!
-//        
-//        // getting date
-//        return range.compactMap { day -> Date in
-//            
-//            return calendar.date(byAdding: .day, value: day - 1, to: startDate)!
-//        }
-//    }
-// }
+ extension Date {
+    
+    func getAllDates() -> [Date] {
+        
+        let calendar = Calendar.current
+        
+        // getting start Date
+        let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
+        
+        let range = calendar.range(of: .day, in: .month, for: startDate)!
+        
+        // getting date
+        return range.compactMap { day -> Date in
+            
+            return calendar.date(byAdding: .day, value: day - 1, to: startDate)!
+        }
+    }
+ }
--- liveOn/liveOn/Views/Calendar/CalendarMain.swift
+++ liveOn/liveOn/Views/Calendar/CalendarMain.swift
@@ -395,26 +395,6 @@ struct CalendarMain: View {
     }
 }
 
-// Extending Date to get Current Month Dates
-extension Date {
-    
-    func getAllDates() -> [Date] {
-        
-        let calendar = Calendar.current
-        
-        // getting start Date
-        let startDate = calendar.date(from: Calendar.current.dateComponents([.year, .month], from: self))!
-        
-        let range = calendar.range(of: .day, in: .month, for: startDate)!
-        
-        // getting date
-        return range.compactMap { day -> Date in
-            
-            return calendar.date(byAdding: .day, value: day - 1, to: startDate)!
-        }
-    }
-}
-
 struct CalendarMain_Previews: PreviewProvider {
     static var previews: some View {
         CalendarBack()
