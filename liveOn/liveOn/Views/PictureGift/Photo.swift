c017d7ff596e97690345b94e68999297cf37f555
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-06-18 02:26:21 +0900

    [Fix] Fix Photo Container List View UI + etc
    PhotoCardsView.swift
    - 서버에서 받아오는 카드는 잠시 주석처리
    - UI를 이루는 패딩 값, 프레임 값 수정
    
    etc
    - Photo.swift에 새 데이터를 추가

--- liveOn/liveOn/Views/PictureGift/Photo.swift
+++ liveOn/liveOn/Views/PictureGift/Photo.swift
@@ -22,6 +22,30 @@ struct PhotoGiftData: Hashable {
 }
 
 // Sample data
-let testData1 = PhotoCardInformation(imageName: "exampleImage1", photoText: "WWDC 오프닝!")
-let testData2 = PhotoCardInformation(imageName: "exampleImage2", photoText: "김치 찌개 맛집")
-let testData3 = PhotoCardInformation(imageName: "picture", photoText: "추억의 00 사진")
+var temporaryData: [PhotoCardInformation] = [
+    
+    PhotoCardInformation(
+        imageName: "exampleImage1",
+        photoText: "WWDC 오프닝!"),
+    
+    PhotoCardInformation(
+        imageName: "photo_picnic",
+        photoText: "날씨 좋다고 즉흥으로 소풍갔던 날"),
+    // 사진 출처 : https://unsplash.com/photos/NdcH-WxzWgo
+    
+    PhotoCardInformation(
+        imageName: "photo_wedding",
+        photoText: "추억의 웨딩 사진"),
+    // 사진 출처 : https://unsplash.com/photos/w5hhoYM_JsU
+    
+    PhotoCardInformation(
+        imageName: "photo_sky",
+        photoText: "별 보러 갔던 날"),
+    // 사진 출처 : https://unsplash.com/photos/5LOhydOtTKU
+    
+    PhotoCardInformation(
+        imageName: "photo_aquarium",
+        photoText: "아쿠아리움에서 해파리만 보다가 온 사진")
+    // 사진 출처 : https://unsplash.com/photos/5LOhydOtTKU
+
+]
--- liveOn/liveOn/Views/PictureGift/PhotoCardsView.swift
+++ liveOn/liveOn/Views/PictureGift/PhotoCardsView.swift
@@ -14,60 +14,71 @@ struct PhotoCardsView: View {
     @State private var loadedImage = ImageGetResponse(createdAt: "", giftPolaroidId: 0, giftPolaroidImage: "", userNickName: "")
     @State private var PhotoGiftDataList: [PhotoGiftData] = [PhotoGiftData(photoURL: "", photoComment: "")]
     
-    var columns: [GridItem] = Array(repeating: GridItem(GridItem.Size.fixed(160)), count: 2)
-    var temporaryData: [PhotoCardInformation] = [testData1, testData2, testData3]
+    var columns: [GridItem] = Array(repeating: GridItem(GridItem.Size.fixed(174)), count: 2)
     
     var body: some View {
         ZStack {
             ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(temporaryData, id: \.self) { data in
+                HStack(alignment: .center, spacing: 12) {
+                    
+                    // 샘플 데이터들이 반복문으로 그려지는 곳
+                    LazyVGrid(columns: columns, spacing: 8) {
+                        ForEach(temporaryData, id: \.self) { data in
+                            
+                            Button(action: {
+                                isTapped.toggle()
+                            }) {
+                                PhotoCard(PhotoCardDetail: data)
+                            }
+                        } // ForEach
+                        
+                        // 전달 받은 사진이 표시되는 곳
+                        // LoadedPhotoCard(imageURL: loadedImage.giftPolaroidImage)
+                        
+                    } // LazyVGrid
+                    .frame(width: .infinity, alignment: .center)
+                    .opacity(isTapped ? 0.2 : 1)
+                    .onTapGesture {
+                        isTapped.toggle()
+                    }
+                } // ScrollView
+                .edgesIgnoringSafeArea(.horizontal)
+                
+                .navigationTitle("")
+                .navigationBarTitleDisplayMode(.inline)
+                .task {
+                    imageGet()
+                    print("이미지 로딩이 수행되었습니다.")
+                    PhotoGiftDataList.append(PhotoGiftData(photoURL: loadedImage.giftPolaroidImage, photoComment: loadedImage.userNickName))
+                }
+                .background(Color.background)
+                .navigationBarBackButtonHidden(true)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
                         Button(action: {
-                            isTapped.toggle()
+                            dismiss()
                         }) {
-                            PhotoCard(PhotoCardDetail: data)
+                            Image(systemName: "chevron.left")
+                                .font(.system(size: 20))
+                                .foregroundColor(.black)
                         }
                     }
-                    LoadedPhotoCard(imageURL: loadedImage.giftPolaroidImage)
-                }
-                .opacity(isTapped ? 0.2 : 1)
-                .onTapGesture {
-                    isTapped.toggle()
-                }
-            }
-            .navigationTitle("")
-            .navigationBarTitleDisplayMode(.inline)
-            .task {
-                imageGet()
-                print("이미지 로딩이 수행되었습니다.")
-                PhotoGiftDataList.append(PhotoGiftData(photoURL: loadedImage.giftPolaroidImage, photoComment: loadedImage.userNickName))
-            }
-            .padding()
-            .background(Color.background)
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button(action: {
-                        dismiss()
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .font(.system(size: 20))
+                    ToolbarItem(placement: .principal) {
+                        Text("사진 선물함")
                             .foregroundColor(.black)
                     }
                 }
-                ToolbarItem(placement: .principal) {
-                    Text("사진 선물함")
-                        .foregroundColor(.black)
-                }
             }
+            
             if isTapped == true {
                 Button(action: {
                     isTapped.toggle()
                 }) {
-                    PhotoCardSheet(PhotoCardDetail: PhotoCardInformation(imageName: "exampleImage2", photoText: "김치찌개가 존맛입니다!!"))
+                    PhotoCardSheet(PhotoCardDetail: temporaryData[1])
                 }
             }
-        }
+            
+        } // body
     }
     
     func imageGet() {
@@ -87,41 +98,58 @@ struct PhotoCardsView: View {
     }
 }
 
+// MARK: PhotoCard
 struct PhotoCard: View {
     
     var PhotoCardDetail: PhotoCardInformation
     
     var body: some View {
         
-        ZStack{
+        ZStack {
             
             // 폴라로이드 연출을 위한 흰 네모
             RoundedRectangle(cornerRadius: 4)
                 .foregroundColor(.white)
                 .shadow(color: .gray, radius: 6, x: 2, y: 2).opacity(0.8)
-                        
+            
             VStack(alignment: .leading, spacing: 5) {
                 
-                Image(PhotoCardDetail.imageName)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.235, alignment: .center)
-                    .padding(.top, 12)
+                // 사진이 들어가는 곳
+                ZStack {
+                    
+                    // TODO: 정해진 네모 프레임 안에 사진 꽉 차게(비율은 그대로 찌그러지지 않게) 할 것
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .opacity(0.88)
+                    
+                    Rectangle()
+                        .foregroundColor(.white.opacity(0.0))
+                        .background(                    Image(PhotoCardDetail.imageName)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .scaledToFit()
+                            .clipped())
+                        .frame(width: 164, height: 192, alignment: .center)
+                }
+                .padding(.top, 10)
+                .padding(.horizontal, 6)
                 
                 VStack(alignment: .leading, spacing: 6) {
                     Rectangle()
                         .foregroundColor(.placeHolderColor)
-                        .frame(width: 80, height: 5)
+                        .frame(width: 88, height: 5)
                         .cornerRadius(15)
                     
                     Rectangle()
                         .foregroundColor(.placeHolderColor)
-                        .frame(width: 60, height: 5)
+                        .frame(width: 66, height: 5)
                         .cornerRadius(15)
                         .padding(.bottom, 6)
+                    
                 } // VStack
                 .padding(.vertical, 6)
                 .padding(.leading, 8)
+                
             } // VStack
         } // ZStack
     } // body
@@ -163,7 +191,9 @@ struct LoadedPhotoCard: View {
 }
 
 struct PhotoCardSheet: View {
+    
     var PhotoCardDetail: PhotoCardInformation
+    
     var body: some View {
         VStack(alignment: .leading) {
             Image(PhotoCardDetail.imageName)
@@ -174,10 +204,13 @@ struct PhotoCardSheet: View {
             Text(PhotoCardDetail.photoText)
                 .foregroundColor(.bodyTextColor)
                 .bold()
-                .padding(.leading, 12)
+                .padding(.leading, 6)
+                .padding(.vertical, 8)
         }
         .frame(width: UIScreen.main.bounds.width * 0.7, height: UIScreen.main.bounds.height * 0.45, alignment: .center)
-        .padding(10)
+        .padding(8)
+        .padding(.top, 4)
+        .padding(.bottom, 12)
         .background(Color.white
             .cornerRadius(4)
             .shadow(color: Color.black.opacity(0.25), radius: 4, x: 0, y: 4))
