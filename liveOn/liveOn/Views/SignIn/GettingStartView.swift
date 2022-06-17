6ebce2ab06c77afd61931e4b94a2584c5f8c27f8
teethemoji <60784937+teethemoji@users.noreply.github.com>
2022-06-17 21:34:21 +0900

    [Fix] Add padding + etc
    - 별명을 정하는 뷰에서 별명을 적는 칸과 글자 카운터가 들어가는 칸 양 옆에 패딩값을 주기
    - ctrl + i 기능을 이용해 들여쓰기 등 코드를 정리

--- liveOn/liveOn/Views/SignIn/GettingStartView.swift
+++ liveOn/liveOn/Views/SignIn/GettingStartView.swift
@@ -13,42 +13,42 @@ struct GettingStartView: View {
     @ObservedObject var authenticationViewModel: AuthenticationViewModel = AuthenticationViewModel()
     
     var body: some View {
-            VStack {
-                
-                Image("TransportSucceed")
-                
-                Spacer()
-                    .frame(height: 60, alignment: .center)
-                    .onTapGesture {
-                        print("임시 다음 버튼 누르셈")
-                    }
-                  .padding()
-                
-                NavigationLink(destination: SetNicknameView().environmentObject(User())) {
-                    Text("임시 다음 버튼")
-                }.onTapGesture {
+        VStack {
+            
+            Image("TransportSucceed")
+            
+            Spacer()
+                .frame(height: 60, alignment: .center)
+                .onTapGesture {
                     print("임시 다음 버튼 누르셈")
                 }
-                .padding(.bottom, 40)
-                
-                SignInWithAppleButton(.signIn, onRequest: {request in request.requestedScopes = []}, onCompletion: {result in authenticationViewModel.didFinishAuthentication(result: result)}
-                )
-                .frame(height: 45)
                 .padding()
+            
+            NavigationLink(destination: SetNicknameView().environmentObject(User())) {
+                Text("임시 다음 버튼")
+            }.onTapGesture {
+                print("임시 다음 버튼 누르셈")
             }
+            .padding(.bottom, 40)
+            
+            SignInWithAppleButton(.signIn, onRequest: {request in request.requestedScopes = []}, onCompletion: {result in authenticationViewModel.didFinishAuthentication(result: result)}
+            )
+            .frame(height: 45)
+            .padding()
+        }
     }
     
     private func showAppleLoginView() {
-
-            let provider = ASAuthorizationAppleIDProvider()
-
-            let request = provider.createRequest()
-
-            request.requestedScopes = [.fullName, .email]
-
-            let controller = ASAuthorizationController(authorizationRequests: [request])
         
-            controller.performRequests()
+        let provider = ASAuthorizationAppleIDProvider()
+        
+        let request = provider.createRequest()
+        
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        
+        controller.performRequests()
         
     }
 }
